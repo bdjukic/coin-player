2b7ab6e85704497101826fe3ebc77ae6481948a7
Bogdan Djukic <bdjukic@gmail.com>
2018-02-28 19:31:01 +0100

    Updating worker.js location

--- demo/processor.js
+++ demo/processor.js
@@ -66,7 +66,7 @@
                 this._asmjsStatus = "loaded";
                 this._startNow()
             }.bind(this), xhr);
-            xhr.open("get", "http://172.19.0.4/worker.js", true);
+            xhr.open("get", "worker.js", true);
             xhr.send()
         } else if (this._asmjsStatus === "unloaded") {
             this._asmjsStatus = "pending";
